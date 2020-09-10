1a9dc53def1b05fbb9950ab3caa9af22b65c2e74
Milda <laiminga.milda@gmail.com>
2020-09-10 20:55:22 +0300

    Refactor calculator

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -9,6 +9,7 @@ namespace CalculatorApp
         const int minVal = 0;
 
         private Stack<int> stack = new Stack<int>();
+
         public IEnumerable<int> Stack
         {
             get
@@ -19,7 +20,7 @@ namespace CalculatorApp
 
         public bool TryPush(int value)
         {
-            if (!InValidRange(value))
+            if (!IsInValidRange(value))
             {
                 return false;
             }
@@ -49,33 +50,21 @@ namespace CalculatorApp
             return TryDoOperation((x, y) => x - y);
         }
 
-        private bool TryDoOperation(Func<int, int, int> func)
+        private bool TryDoOperation(Func<int, int, int> binaryOperation)
         {
             if (stack.Count < 2)
             {
                 return false;
             }
-            int result = func(stack.Pop(),stack.Pop());
-            stack.Push(ReduceResult(result));
+            int result = binaryOperation(stack.Pop(),stack.Pop());
+            stack.Push(Normalize(result));
             return true;
         }
 
-        private bool InValidRange(int value)
-        {
-            return value >= minVal && value <= maxVal;
-        }
+        private bool IsInValidRange(int value) 
+            => value >= minVal && value <= maxVal;
 
-        private int ReduceResult(int result)
-        {            
-            if (!InValidRange(result))
-            {
-                result = result % (maxVal + 1);
-                if (result < 0)
-                {
-                    result += maxVal + 1;
-                }
-            }
-            return result;
-        }
+        private int Normalize(int result)
+            => result & maxVal;
     }
 }
